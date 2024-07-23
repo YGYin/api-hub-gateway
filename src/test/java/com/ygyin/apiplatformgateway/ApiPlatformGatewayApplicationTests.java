a9671d0aa1161fd028bd4d069dfb86630e8b4348
YGYin <ygyin0307@gmail.com>
2024-07-23 16:36:39 +0800

    Sample test

---
+++ src/test/java/com/ygyin/apiplatformgateway/ApiPlatformGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygyin.apiplatformgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlatformGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
